f34a3f3abdcc39a5ddebfd6166fca7ce2835fbbe
Sukarno Sukarno <sukarno.zhang@gmail.com>
2021-06-19 17:43:13 +0800

    first commit

---
+++ screens/AccountScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { commonStyles } from "../styles/commonStyles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+export default function AccountScreen({ navigation }) {
+  function signOut() {
+    AsyncStorage.removeItem("token");
+    navigation.navigate("SignIn");
+  }
+
+  return (
+    <View style={commonStyles.container}>
+      <Text>Account Screen</Text>
+      <Button title="Sign out" onPress={signOut} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/SignInScreen.js
@@ -0,0 +1,117 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import axios from "axios";
+
+const API = "https://sukarno.pythonanywhere.com";
+const API_LOGIN = "/auth";
+
+export default function SignInScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorText, setErrorText] = useState("");
+
+  async function login() {
+    console.log("---- Login time ----");
+    Keyboard.dismiss();
+
+    try {
+      const response = await axios.post(API + API_LOGIN, {
+        username,
+        password,
+      });
+      console.log("Success logging in!");
+      console.log(response);
+
+      AsyncStorage.setItem("token", response.data.access_token);
+      navigation.navigate("Account");
+    } catch (error) {
+      console.log("Error logging in!");
+      console.log(error.response);
+
+      setErrorText(error.response.data.description);
+    }
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sign in to blog</Text>
+        <Text style={styles.fieldTitle}>Username</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={username}
+          onChangeText={(input) => setUsername(input)}
+        />
+        <Text style={styles.fieldTitle}>Password</Text>
+        <TextInput
+          style={styles.input}
+          autoCapitalize="none"
+          autoCompleteType="password"
+          autoCorrect={false}
+          secureTextEntry={true}
+          value={password}
+          onChangeText={(input) => setPassword(input)}
+        />
+        <TouchableOpacity onPress={login} style={styles.loginButton}>
+          <Text style={styles.buttonText}>Log in</Text>
+        </TouchableOpacity>
+        <Text style={styles.errorText}>{errorText}</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 24,
+  },
+  title: {
+    fontSize: 36,
+    fontWeight: "bold",
+    marginBottom: 24,
+  },
+  fieldTitle: {
+    fontSize: 18,
+    marginBottom: 12,
+  },
+  input: {
+    borderColor: "#999",
+    borderWidth: 1,
+    marginBottom: 24,
+    padding: 4,
+    height: 36,
+    fontSize: 18,
+    backgroundColor: "white",
+  },
+  loginButton: {
+    backgroundColor: "blue",
+    width: 120,
+    alignItems: "center",
+    padding: 18,
+    marginTop: 12,
+    marginBottom: 36,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  errorText: {
+    color: "red",
+    height: 40,
+  },
+});
